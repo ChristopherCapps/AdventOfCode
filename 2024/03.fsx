20704b8d2af30d5c1f7c48141d9b11b95d7330e5
chris.capps@live.com <chris@the-lamp-post.lan>
2024-12-04 08:53:07 -0500

    Finished Day 3

--- 2024/03.fsx
+++ 2024/03.fsx
@@ -1,4 +1,4 @@
-let input = System.IO.File.ReadAllLines "03.input" 
+let input = System.IO.File.ReadAllLines "2024/03.input" 
 
 let lines = input
 
@@ -12,7 +12,7 @@ let consume_digits (s: string) =
     //printfn "Result: %O" result
     match result with
     | [] -> None
-    | _ as digits -> (System.String (Array.ofList digits)) |> int |> Some
+    | _ as digits -> (System.String (Array.ofList digits)) |> Some
 
 let consume_mul (s: string) =
     s.Length >= 4 && s.Substring(0, 4).Equals("mul(")
@@ -23,3 +23,56 @@ let consume_comma (s: string) =
 let consume_close_parenthesis (s:string) =
     s.Length >= 1 && s.[0] = ')'
 
+let consume_do (s:string) =
+    s.Length >= 4 && s.StartsWith("do()")
+
+let consume_dont (s:string) =
+    s.Length >= 7 && s.StartsWith("don't()")
+
+let consume_line (s: string) (start_enabled: bool)=
+    let mutable s' = s;
+    let mutable products = []    
+    let mutable enabled = start_enabled
+    while (s'.Length >= 8) do
+        if s' |> consume_mul then
+            s' <- s'.Substring(4)
+            match s' |> consume_digits with
+            | Some digits -> 
+                s' <- s'.Substring(digits.Length)
+                if s' |> consume_comma then
+                    s' <- s'.Substring(1)
+                    match s' |> consume_digits with
+                    | Some digits' -> 
+                        s' <- s'.Substring(digits'.Length)
+                        if s' |> consume_close_parenthesis then
+                            s' <- s'.Substring(1) // advance 1 char
+                            if enabled then products <- products @ [(int digits) * (int digits')]
+                        else s' <- s'.Substring(1) // Couldn't consume ")", so move to next char
+                    | None -> s' <- s'.Substring(1) // Couldn't consume a digit, so move to next char
+                else s' <- s'.Substring(1) // Couldn't consume ",", so move to next char
+            | None -> s' <- s'.Substring(1) // Couldn't consume a digit, so move to next char
+        else 
+            if s' |> consume_do then 
+                enabled <- true
+                s' <- s'.Substring(4) // Consume do()
+            else if s' |> consume_dont then
+                enabled <- false
+                s' <- s'.Substring(7) // Consume don't()
+            else 
+                s' <- s'.Substring(1) // Couldn't consume mul, do, don't so advance 1
+    products, enabled
+
+let mutable enabled = true
+let mutable products = []
+
+for line in lines do
+    let products', enabled' = consume_line line enabled
+    enabled <- enabled'
+    products <- products @ products'
+
+products |> List.reduce (+)
+
+// lines 
+// |> Seq.map consume_line
+// |> Seq.map (List.reduce (+))
+// |> Seq.reduce (+)
